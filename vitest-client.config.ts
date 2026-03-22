b5d95d80be0c00cbf992c63c63686a8d5c93064c
Teages <Teages@teages.xyz>
2026-03-22 21:57:05 +0800

    fix: close nitro runner after tests finish on client test

--- vitest-client.config.ts
+++ vitest-client.config.ts
@@ -1,7 +1,11 @@
 import { defineConfig, mergeConfig } from 'vitest/config'
+import { nitroTestPlugin } from './test/plugin.ts'
 import viteConfig from './vite.config.ts'
 
 export default mergeConfig(viteConfig, defineConfig({
+  plugins: [
+    nitroTestPlugin(), // close nitro runner after tests finish
+  ],
   test: {
     include: [
       'test/app/**/*.test.ts',
